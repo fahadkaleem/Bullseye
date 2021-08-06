1738856f26c31daf4c34f21745252a6e92b5b35f
mohammedfahadkaleem <mohammedfahadkaleem@gmail.com>
2021-08-06 23:56:17 +0530

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Mohammed Fahad Kaleem on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
